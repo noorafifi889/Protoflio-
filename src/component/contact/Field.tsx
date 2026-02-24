8b1c3af373a580125758996f5e5a88b5c686d45e
noorafifi889 <noorelafifi889@gmail.com>
2026-02-24 21:17:07 +0200

    add a neme attribute

--- src/component/contact/Field.tsx
+++ src/component/contact/Field.tsx
@@ -2,11 +2,12 @@ export function Field({
   label,
   placeholder,
   type,
+  name
 }: {
   label: string;
   placeholder: string;
   type: string;
-  name?: string;
+  name: string;
 }) {
   return (
     <div>
@@ -14,6 +15,7 @@ export function Field({
         {label}
       </label>
    <input
+   name={name}
   type={type}
   placeholder={placeholder}
   className="w-full rounded-xl border border-black/10 bg-white px-4 py-3 mb-3 text-sm text-slate-900 outline-none placeholder:text-slate-400 focus:outline-none focus:ring-0 focus:border-black/10 dark:border-white/10 dark:bg-white/[0.04] dark:text-white dark:placeholder:text-white/35 dark:focus:border-white/10"
